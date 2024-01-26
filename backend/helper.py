ebb84503976ae7b1d153c28450caa0ef355df01b
SohamDesai1 <sohamdesai123@gmail.com>
2024-01-26 23:58:02 +0530

    chore(amazon): changed the headers and some different selectors

--- backend/helper.py
+++ backend/helper.py
@@ -6,35 +6,42 @@ from bs4 import BeautifulSoup
 
 def scrap_amazon(name):
     try:
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"}
-        URL = "https://www.amazon.in/s?k=" + \
+        headers = { 
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36', 
+        'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
+        'Accept-Language' : 'en-US,en;q=0.5',
+        'Accept-Encoding' : 'gzip', 
+        'DNT' : '1', # Do Not Track Request Header 
+        'Connection' : 'close'
+        }
+        url_a = "https://www.amazon.in/s?k=" + \
             str(name) + "&ref=nb_sb_noss_2"
-        recieve = requests.get(URL, headers=headers)
-        soup = BeautifulSoup(recieve.content, 'html.parser')
-
-        title = soup.find(
+        recieve_a = requests.get(url_a, headers=headers)
+        # print(recieve.content)
+        soup = BeautifulSoup(recieve_a.content, 'html.parser')
+        title_a = soup.find(
             "span", {"class": "a-size-medium a-color-base a-text-normal"}).text
-
-        parent_price = soup.find(
+        parent_price_a = soup.find(
             'div', class_='a-row a-size-base a-color-base')
-        price = parent_price.find('span', class_='a-price-whole').text
-        image = soup.find('img', attrs={'class': 's-image'})
-        imagelink = image['src']
-        rating_text = soup.find('i', attrs={
-                                'class': 'a-icon a-icon-star-small a-star-small-4-5 aok-align-bottom'}).text
-        parts = rating_text.split(" out of ")
-        rating = parts[0]
-        reviews = soup.find(
+        price_a = parent_price_a.find('span', class_='a-price-whole').text
+        print("Amazon:", price_a)
+        image_a = soup.find('img', attrs={'class': 's-image'})
+        imagelink_a = image_a['src']
+        print(imagelink_a)
+        rating_text_a = soup.find('span', attrs={
+                                'class': 'a-icon-alt'}).text
+        parts_a = rating_text_a.split(" out of ")
+        rating_a = parts_a[0]
+        reviews_a = soup.find(
             "span", {"class": "a-size-base s-underline-text"}).text
-        return jsonify({"name": title, "price": price, "image": imagelink, "rating": rating, "reviews": reviews})
+        return jsonify({"name": title_a, "price": price_a, "image": imagelink_a, "rating": rating_a, "reviews": reviews_a})
     except:
         return jsonify({"error": "Not Found"})
 
 
 def scrap_flipkart(name):
     try:
-        headers = {
+        headers_f = {
             'Connection': 'keep-alive',
             'Cache-Control': 'max-age=0',
             'Upgrade-Insecure-Requests': '1',
@@ -46,22 +53,23 @@ def scrap_flipkart(name):
             'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'en-GB,en;q=0.9,en-US;q=0.8,nl;q=0.7',
         }
-        url = "https://www.flipkart.com/search?q=" + str(
+        url_f = "https://www.flipkart.com/search?q=" + str(
             name) + "&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
-        r = requests.get(url, headers=headers)
-        r = r.text
-        soup = BeautifulSoup(r, 'lxml')
-        title = soup.find('div', {'class': '_4rR01T'}).text
-        price = soup.find('div', {'class': '_30jeq3 _1_WHN1'}
+        r_f = requests.get(url_f, headers=headers_f)
+        r_f = r_f.text
+        soup_f = BeautifulSoup(r_f, 'lxml')
+        title_f = soup_f.find('div', {'class': '_4rR01T'}).text
+        price_f = soup_f.find('div', {'class': '_30jeq3 _1_WHN1'}
                           ).text.replace("₹", "")
-        image = soup.find('img', attrs={'class': '_396cs4'})
-        imagelink = image['src']
-        rating = soup.find('div', {'class': '_3LWZlK'}).text
-        reviews = soup.find('span', {'class': '_2_R_DZ'}).text
-        parts = reviews.split('&')
-        reviews_part = parts[1]
-        reviews = reviews_part.strip().split()[0]
-        return jsonify({"name": title, "price": price, "image": imagelink, "rating": rating, "reviews": reviews})
+        print(price_f)
+        image_f = soup_f.find('img', attrs={'class': '_396cs4'})
+        imagelink_f = image_f['src']
+        rating_f = soup_f.find('div', {'class': '_3LWZlK'}).text
+        reviews_f = soup_f.find('span', {'class': '_2_R_DZ'}).text
+        parts_f = reviews_f.split('&')
+        reviews_part_f = parts_f[1]
+        reviews_f = reviews_part_f.strip().split()[0]
+        return jsonify({"name": title_f, "price": price_f, "image": imagelink_f, "rating": rating_f, "reviews": reviews_f})
 
     except:
         return jsonify({"error": "Not Found"})
