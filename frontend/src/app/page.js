efbf9ccd42ba6c14135a676c1fd41c076c84c9c2
SohamDesai1 <sohamdesai123@gmail.com>
2024-03-12 21:31:54 +0530

    style: Improved home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,13 +1,15 @@
 "use client"
 
 import Link from "next/link";
+import Compare from "./components/Compare";
 
 export default function Home() {
   return (
     <>
       <section className="text-white bg-gray-900">
-        <div className="max-w-screen-xl px-4 py-32 mx-auto lg:flex lg:h-screen lg:items-center">
-          <div className="max-w-3xl mx-auto text-center">
+        <div className="max-w-screen-xl px-4 py-32 mx-auto lg:flex lg:h-[88.4vh] lg:items-center">
+          <img src="/bg.png" alt="" className="relative "/>
+          <div className="absolute max-w-3xl mx-auto text-center top-[40%] left-[30%]">
             <h1 className="text-3xl font-extrabold text-transparent bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text sm:text-5xl">
               TRackky.
               <br />
@@ -18,23 +20,19 @@ export default function Home() {
               Through the power of Web Scrapping we bring you the one stop tool for your next E-Commerce purchase.
             </p>
             <div className="flex flex-wrap justify-center gap-4 mt-8">
-              <Link
-                className="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-                href="/comparison"
-              >
+              <div className="justify-center px-10 py-5 text-2xl text-center text-gray-900 whitespace-nowrap bg-cyan-400 rounded-[36px]">
                 Get Started
-              </Link>
-              <a
-                className="block w-full px-12 py-3 text-sm font-medium text-white border border-blue-600 rounded hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
-                href="/about"
-              >
-                Learn More
-              </a>
+              </div>
+              <div className="justify-center px-10 py-5 text-2xl text-center text-cyan-400 whitespace-nowrap border border-cyan-400 border-solid rounded-[36px]">
+                Learn more
+              </div>
             </div>
           </div>
         </div>
+        {/* <Footer /> */}
       </section>
-
+      
+       {/* <Compare /> */}
     </>
   );
 }
