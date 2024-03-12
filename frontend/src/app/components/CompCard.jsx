ea503d983a8f9d548e26e7486fe0aa5b42e4440c
SohamDesai1 <sohamdesai123@gmail.com>
2024-03-12 21:33:12 +0530

    style(compare): Made a compare card and added in the compare component

---
+++ frontend/src/app/components/CompCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Rating } from 'react-custom-rating-component'
+
+const CompCard = ({ name, image, price, rating, reviews, website }) => {
+    return (
+        <div className="flex flex-col items-center px-11 py-14 bg-white border-yellow-400 border-solid shadow-lg border-[7px] max-w-[467px] rounded-[33px]">
+            <img
+                src={image}
+                loading="lazy"
+                srcSet="..."
+                className="max-w-full shadow-sm aspect-[0.79] w-[262px]"
+            />
+            <div className="mt-6 text-4xl font-bold whitespace-nowrap text-blue-950">
+                {name}
+            </div>
+            <div className="mt-6 text-4xl font-bold text-blue-950">{website}</div>
+            <div className="mt-6 text-4xl font-bold text-blue-950">{price}</div>
+            <div className="mt-6 text-4xl font-bold text-blue-950">{reviews} reviews</div>
+            <Rating
+                defaultValue={rating}
+                size='30px'
+                spacing='10px'
+                activeColor='yellow'
+            />
+        </div>
+    )
+}
+
+export default CompCard
--- frontend/src/app/components/Compare.jsx
+++ frontend/src/app/components/Compare.jsx
@@ -1,57 +1,14 @@
 "use client"
 import Link from 'next/link'
 import React from 'react'
+import CompCard from './CompCard'
 
 const Compare = ({ name_amzn, price_amzn, image_amzn, rating_amzn, reviews_amzn, name_flip, price_flip, image_flip, rating_flip, reviews_flip }) => {
     return (
         <>
-            <div className="flex flex-col items-center justify-center bg-gradient-to-bl from-blue-50 to-violet-50">
-                <div className="p-4 ">
-                    <div className="flex gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
-                        <div className="flex flex-col items-center justify-center p-4 bg-white border rounded-lg">
-                            <div className="mb-2 text-xl font-bold">Amazon</div>
-                            <img
-                                src={image_amzn}
-                                alt="Placeholder Image"
-                                className="w-full h-auto rounded-md"
-                            />
-                            <div className="px-1 py-4 text-center">
-                                <div className="mb-2 text-xl font-bold">{name_amzn}</div>
-
-                                <p className="text-base text-gray-700">
-                                    Price : {price_amzn}
-                                </p>
-                                <p className="text-base text-gray-700">
-                                    Rating : {rating_amzn}
-                                </p>
-                                <p className="text-base text-gray-700">
-                                    Reviews : {reviews_amzn}
-                                </p>
-                            </div>
-                        </div>
-                        <div className="flex flex-col items-center justify-center p-4 bg-white border rounded-lg">
-                            <div className="mb-2 text-xl font-bold">Flipkart</div>
-                            <img
-                                src={image_flip}
-                                alt="Placeholder Image"
-                                className="object-fill w-full h-auto rounded-md"
-                            />
-                            <div className="px-1 py-4 text-center">
-                                <div className="mb-2 text-xl font-bold">{name_flip}</div>
-
-                                <p className="text-base text-gray-700">
-                                    Price : {price_flip}
-                                </p>
-                                <p className="text-base text-gray-700">
-                                    Rating : {rating_flip}
-                                </p>
-                                <p className="text-base text-gray-700">
-                                    Reviews : {reviews_flip}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className='flex items-center content-center justify-center h-screen gap-6'>
+                <CompCard name={name_amzn} image={image_amzn} price={price_amzn} rating={rating_amzn} reviews={reviews_amzn} website={"Amazon"} />
+                <CompCard name={name_flip} image={image_flip} price={price_flip} rating={rating_flip} reviews={reviews_flip} website={"Flipkart"} />
             </div>
         </>
 
