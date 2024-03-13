81e8008658cfa4b0067bae254a09ff3770c44ffa
SohamDesai1 <sohamdesai123@gmail.com>
2024-03-14 02:14:09 +0530

    style(favourites): Created card component

--- frontend/src/app/components/FavCard.jsx
+++ frontend/src/app/components/FavCard.jsx
@@ -3,17 +3,36 @@ import Link from "next/link"
 const FavCard = ({ name, a_price, f_price, image }) => {
     return (
         <>
-            <div className="items-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <img className="w-56 h-64 rounded-t-lg " src={image} alt="" />
-                <div className="p-5">
-                    <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
-                    </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Amazon Price : {a_price}</p>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Flipkart Price : {f_price}</p>
-                    <Link href={"/chart"}> <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Price Chart
-                    </a></Link>
+            <div className="flex flex-col items-center px-7 pt-5 pb-16 font-bold bg-gradient-to-r from-[#033A4C] via-[#03063D] to-[#170074] text-white rounded-3xl max-w-[465px]">
+                <img
+                    loading="lazy"
+                    src= {image}
+                    className="self-stretch w-full aspect-[1.3]"
+                />
+                <div className="mt-4 text-2xl text-center underline">
+                    {name}
+                </div>
+                <div className="mt-12 text-2xl whitespace-nowrap">Current price :</div>
+                <div className="flex justify-between gap-5 mt-9 whitespace-nowrap">
+                    <div className="text-2xl grow">Flipkart</div>
+                    <img
+                        loading="lazy"
+                        src="arrow.png"
+                        className="shrink-0 self-center mt-1 aspect-[5.56] stroke-[1px] stroke-white w-[45px]"
+                    />
+                    <div className="my-auto text-lg grow">Rs.{f_price}</div>
+                </div>
+                <div className="flex justify-between gap-5 mt-3 whitespace-nowrap">
+                    <div className="text-2xl grow">Amazon</div>
+                    <img
+                        loading="lazy"
+                        src="arrow.png"
+                        className="shrink-0 self-center mt-1 aspect-[5.56] stroke-[1px] stroke-white w-[45px]"
+                    />
+                    <div className="text-lg grow">Rs.{a_price}</div>
+                </div>
+                <div className="flex gap-3.5 px-5 py-3.5 mt-14 text-lg font-semibold text-black bg-cyan-400 rounded-2xl">
+                    <Link href={"/chart"}> <div className="flex-auto">Check Graph</div></Link>
                 </div>
             </div>
         </>
