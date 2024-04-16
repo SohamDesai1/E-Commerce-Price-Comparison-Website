dbd9fa8e133446643a76731e851d032297b1b631
SohamDesai1 <sohamdesai123@gmail.com>
2024-04-16 23:49:00 +0530

    style: Improved chart page

--- frontend/src/app/chart/page.js
+++ frontend/src/app/chart/page.js
@@ -20,14 +20,16 @@ ChartJS.register(
     Tooltip
 );
 const MyChart = () => {
-    const [array, setArray] = useState([])
+    const [array_a, setArray_a] = useState([])
+    const [array_f, setArray_f] = useState([])
 
     const getFavorites = async () => {
         try {
             const response = await axios.get("/api/favourites");
             if (response.data) {
                 console.log(response.data.favouriteProducts[0])
-                setArray(response.data.favouriteProducts[0]["a_price_change"])
+                setArray_a(response.data.favouriteProducts[0]["a_price_change"])
+                setArray_f(response.data.favouriteProducts[0]["f_price_change"])
             }
         } catch (error) {
             console.log(error);
@@ -46,18 +48,30 @@ const MyChart = () => {
             },
             title: {
                 display: true,
-                text: 'Chart.js Line Chart',
+                text: 'Price Line Chart',
             },
         }
     };
-    const labels = ['November', 'December', 'January', 'Febuary', 'May', 'June', 'July'];
+    const labels = ['August', 'September', 'October', 'November', 'December', 'January', 'Febuary', 'March'];
 
-    const data = {
+    const data_a = {
         labels,
         datasets: [
             {
                 label: 'Iphone 15',
-                data: array,
+                data: array_a,
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+        ],
+    };
+
+    const data_f = {
+        labels,
+        datasets: [
+            {
+                label: 'Iphone 15',
+                data: array_f,
                 borderColor: 'rgb(255, 99, 132)',
                 backgroundColor: 'rgba(255, 99, 132, 0.5)',
             },
@@ -65,11 +79,17 @@ const MyChart = () => {
     };
 
     return (
-        <div>
+        <div className="bg-gradient-to-r from-[#010003] via-[#221C4C] to-[#002D2B] max-md:px-5 ">
+            <div className="text-6xl text-white font-customfont"> Amazon</div>
             <div className="w-[1100px] h-screen flex mx-auto my-auto">
-                <div className='w-full pt-0 my-auto border border-gray-400 shadow-xl rounded-xl h-fit'>
-
-                    <Line options={options} data={data} />
+                <div className='w-full pt-0 my-auto bg-white border border-gray-400 shadow-xl rounded-xl h-fit'>
+                    <Line options={options} data={data_a} />
+                </div>
+            </div>
+            <div className="text-6xl text-white font-customfont"> Flipkart</div>
+            <div className="w-[1100px] h-screen flex mx-auto my-auto">
+                <div className='w-full pt-0 my-auto bg-white border border-gray-400 shadow-xl rounded-xl h-fit'>
+                    <Line options={options} data={data_f} />
                 </div>
             </div>
         </div>
