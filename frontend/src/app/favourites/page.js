b2f36377cfac55073aa24e372040228f97782189
SohamDesai1 <sohamdesai123@gmail.com>
2024-04-27 00:03:47 +0530

    style(favourites): added a description comp

--- frontend/src/app/favourites/page.js
+++ frontend/src/app/favourites/page.js
@@ -22,25 +22,35 @@ const Favourites = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Your Favourites</h1>
-            {favourites && favourites.length == 0 ? (
-                <p>No favorite products yet.</p>
-            ) : (
-                <>
-                    {/* the card shouldbe in a row of 3 */}
-                    <div className="grid grid-cols-3 gap-4">
-                        {favourites.map((product) => (
-                            <div key={product._id}>
-                                <FavCard name={product.name} image={product.image} f_price={product.f_price} a_price={product.a_price} />
-                            </div>
-                        ))}
+        <>
+            <div className='bg-[rgb(223,249,355)]'>
+                <div class="container px-5 py-24 mx-auto ">
+                    <div class="flex flex-wrap w-full mb-20">
+                        <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+                            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Your Favourites</h1>
+                            <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+                        </div>
+                        <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Your products at one place to check the pricing and compare the prices.</p>
                     </div>
+                </div>
+                {favourites && favourites.length == 0 ? (
+                    <p>No favorite products yet.</p>
+                ) : (
+                    <>
+                        {/* the card shouldbe in a row of 3 */}
+                        <div className="grid grid-cols-3 gap-4">
+                            {favourites.map((product) => (
+                                <div key={product._id}>
+                                    <FavCard name={product.name} image={product.image} f_price={product.f_price} a_price={product.a_price} />
+                                </div>
+                            ))}
+                        </div>
 
-                </>
-            )}
+                    </>
+                )}
 
-        </div>
+            </div>
+        </>
     );
 };
 
